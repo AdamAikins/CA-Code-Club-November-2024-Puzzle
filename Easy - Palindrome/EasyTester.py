fbe7ba713a8bd5594e26a34a2d7e11aad2ffd9c1
jaydentsao <164285583+jaydentsao@users.noreply.github.com>
2024-10-31 22:19:48 -0400

    Update EasyTester.py

---
+++ Easy - Palindrome/EasyTester.py
@@ -0,0 +1,10 @@
+def is_palindrome(input_str):
+    # Write code here
+    return False  # Delete when done
+
+print(is_palindrome("racecar"))  # True
+print(is_palindrome("codeclub"))  # False
+print(is_palindrome("codeedoc"))  # True
+print(is_palindrome(""))  # True
+print(is_palindrome("a"))  # True
+print(is_palindrome("yv"))  # False
